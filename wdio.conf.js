f7676dc441b37a62beee9bbf08436a330185f114
RomaVezhel <romavezhel@gmail.com>
2021-05-06 15:31:41 +0300

    added maximizeWindow

--- wdio.conf.js
+++ wdio.conf.js
@@ -188,8 +188,9 @@ exports.config = {
      * @param {Array.<String>} specs        List of spec file paths that are to be run
      * @param {Object}         browser      instance of created browser/device session
      */
-    // before: function (capabilities, specs) {
-    // },
+    before: async function (capabilities, specs) {
+        await browser.maximizeWindow();
+    },
     /**
      * Runs before a WebdriverIO command gets executed.
      * @param {String} commandName hook command name
